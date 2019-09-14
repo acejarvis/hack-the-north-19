0d49b42b287e594ad4a2a97ed7d37b01ebe134fd
Helena Ye <yex@mcmaster.ca>
2019-09-14 16:15:58 -0400

    add count and expense on a specific store location

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request-promise-native'); // use Request library + promises to reduce lines of code
+const simpleHashTable = require('simple-hashtable');
+
 
 const apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiNjU0MWQyNjQtMTgwZS0zNTVkLTg2ZmUtMGJiNjNmYmRmMmRiIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiIyODA5YmQzMC03N2I5LTQ4ZTItYjVmYy1lZjQ2NTgxMmQ4YjIifQ.syA-Drtn7SFiiZJv_apCF7hIMMjqYVMH5yID449JLws";
 //const initialCustomerId = "cad31095-cae1-49f4-bea8-78c42e2d92b7";
@@ -45,14 +47,29 @@ router.get('/:customerId', async (req, res)=>{
     
       var x = [];  
       console.log("asdfasfs " + resp.result[0].id);
-      
+      let hashTable = new simpleHashTable();
       //getting a bunch of tranid
       for(var i = 0; i < resp.result.length;i++){
        let tmpLocation = {
          "longitude": resp.result[i].locationLongitude,
          "latitude": resp.result[i].locationLatitude
        };
-       x.push(tmpLocation);
+       if(hashTable.get(tmpLocation) === -1){
+         //new location
+         hashTable.put(JSON.stringify(tmpLocation), {
+           "count": 1,
+           "expense": Math.abs(resp.result[i].currencyAmount)
+         });
+       }else{
+         //same location already exists in hash table
+         let tmp = hashTable.get(JSON.stringify(tmpLocation));
+         tmp.count = tmp.count +1;
+         tmp.expense = tmp.expense + Math.abs(resp.result[i].currencyAmount);
+         console.log(JSON.stringify(tmp));
+       }
+
+      //  x.push(tmpLocation);
+        
       }
    
       console.log(x);
